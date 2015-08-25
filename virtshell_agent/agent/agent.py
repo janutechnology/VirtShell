3140fee7a189ebce10e19d113c46d51e44924129
Carlos Llano <carlos_llano@hotmail.com>
2015-08-25 01:07:02 -0500

    Changed to process because sqlite has problems with threads

--- virtshell_agent/agent/agent.py
+++ virtshell_agent/agent/agent.py
@@ -11,14 +11,15 @@ import os
 import sys
 import imp
 import time
+import json
 import signal
+import psutil
 import os.path
 import logging
 import datetime
-import threading
-import simplejson
 import tornado.web
 import tornado.ioloop
+import multiprocessing
 import tornado.websocket
 import tornado.httpserver
 from database import Database
@@ -34,10 +35,12 @@ database = None
 # Signals
 ################################################################################
 def signal_handler(signal, frame):
-    if agent_service.is_alive():
-        agent_service._Thread__stop()
-        main_logger.info("agent-service terminated.")
-        main_logger.info("terminated.")
+    pid = os.getpid()
+    parent = psutil.Process(pid)
+    for child in parent.children(recursive=True):
+        main_logger.info("agent-service with pid [%d] terminated.", child.pid)
+        child.kill()
+    main_logger.info("agent with pid [%d] terminated.", pid)
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
@@ -45,22 +48,29 @@ signal.signal(signal.SIGTERM, signal_handler)
 ################################################################################
 # AgentService Class
 ################################################################################
-class AgentService(threading.Thread):
-    def __init__(self, database): 
-        threading.Thread.__init__(self) 
+class AgentService(multiprocessing.Process):
+    def run(self):
         self.logger = init_logger('virtshell-agent-service')
         self.plugins = {}
         self.plugin_folder = "./plugins"
         self.special_method = "__init__.py"
-        self.database = database
+        self.database = Database(self.logger)
         self.logger.info("started...")
-
-    def run(self):
         self.get_plugins()
-        while True:
-            self.logger.info("en el hilo....")
-            time.sleep(10)
 
+        while True:
+            request = self.database.get_request()
+            if request != None:
+                message = request.message
+                plugin = self.load_plugin(message['type'])
+                result = plugin.run(self.logger, message)
+                if result == 0:
+                    request.status = 1
+                    self.database.update_request(request)
+            else:
+                time.sleep(10)
+            self.logger.info("en el hilo....")
+            
     def get_plugins(self):
         self.logger.info("finding plugins")
         possible_plugins = os.listdir(self.plugin_folder)
@@ -72,8 +82,8 @@ class AgentService(threading.Thread):
                 self.plugins[module] = info
                 self.logger.info("plugin %s.py found" % module)
 
-    def load_plugin(name):
-        return imp.load_module(name, *(self.plugins[name]))    
+    def load_plugin(self, name):
+        return imp.load_module(name, *(self.plugins[name]))
 
 ################################################################################
 # CreateInstanceHandler Class
@@ -87,12 +97,9 @@ class CreateInstanceHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self, json_message):
         main_logger.info("message received %s" % json_message)
-        message = simplejson.loads(json_message)
-        print message
+        #message = simplejson.loads(json_message)
+        message = json.load(json_message)
         database.insert_new_request(message)
-        # if message['action'] == 'create':
-        #     plugin = load_plugin(message['type'])
-        #     plugin.run()
         self.write_message("received")
 
     def on_close(self):
@@ -101,7 +108,6 @@ class CreateInstanceHandler(tornado.websocket.WebSocketHandler):
 
     @classmethod
     def write_to_clients(cls):
-        print "Writing to clients"
         for client in cls.clients:
             client.write_message("Hi there!")
 
@@ -139,7 +145,7 @@ def init_logger(LoggerName):
 if __name__ == "__main__":
     main_logger = init_logger('virtshell-agent')
     database = Database(main_logger)
-    agent_service = AgentService(database)
+    agent_service = AgentService()
     agent_service.start()
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(8080)
