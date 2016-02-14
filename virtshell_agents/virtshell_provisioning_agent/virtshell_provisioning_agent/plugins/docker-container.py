40a693137a9cce02740678f0cf66eaa1fe58050f
Carlos Llano <carlos_llano@hotmail.com>
2016-02-14 11:11:45 -0500

    Updated docker-container plugin

--- virtshell_agents/virtshell_provisioning_agent/virtshell_provisioning_agent/plugins/docker-container.py
+++ virtshell_agents/virtshell_provisioning_agent/virtshell_provisioning_agent/plugins/docker-container.py
@@ -30,7 +30,7 @@ def init_logger(LoggerName):
 logger = init_logger('docker-container')
 database = Database(logger)
 
-def create(request):        
+def create(request):
     try:
         database.insert_new_request(request)
         create_daemon = threading.Thread(name = 'create_daemon',
@@ -97,42 +97,43 @@ def _create_container(request):
 
             logger.info(message_log)
 
+            client = Client(version='1.20', base_url='tcp://127.0.0.1:2376')
+
+            distribution = "ubuntu:14.04"
+
             dockerfile = '''
-                FROM ubuntu:14.04
-                MAINTAINER Carlos Llano <carlos_llano@hotmail.com>
-                RUN sed 's/#$ModLoad imudp/$ModLoad imudp/' -i /etc/rsyslog.conf
-                RUN sed 's/#$UDPServerRun 514/$UDPServerRun 514/' -i /etc/rsyslog.conf
-                RUN sed 's/#$ModLoad imtcp/$ModLoad imtcp/' -i /etc/rsyslog.conf
-                RUN sed 's/#$InputTCPServerRun 514/$InputTCPServerRun 514/' -i /etc/rsyslog.conf
-
-                EXPOSE 514/tcp 514/udp 
-                CMD ["/usr/sbin/rsyslogd", "-dn", "-f", "/etc/rsyslog.conf"]
-
-                RUN apt-get install -y openssh-server
-                RUN mkdir /var/run/sshd
-
-                RUN useradd -s /bin/bash -m virtshell
-                RUN echo "virtshell:virtshell" | chpasswd
-                RUN echo "virtshell ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-                RUN echo "root:root" | chpasswd
-                RUN sed -i "s/PermitRootLogin without-password/PermitRootLogin yes/" /etc/ssh/sshd_config
-
-                RUN sed "s@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g" -i /etc/pam.d/sshd
-                ENV NOTVISIBLE "in users profile"
-                RUN echo "export VISIBLE=now" >> /etc/profile
-                EXPOSE 22
-                CMD ["/usr/sbin/sshd", "-D"]
+            FROM ''' + distribution + '''
+            MAINTAINER Carlos Llano <carlos_llano@hotmail.com>
+            RUN sed 's/#$ModLoad imudp/$ModLoad imudp/' -i /etc/rsyslog.conf
+            RUN sed 's/#$UDPServerRun 514/$UDPServerRun 514/' -i /etc/rsyslog.conf
+            RUN sed 's/#$ModLoad imtcp/$ModLoad imtcp/' -i /etc/rsyslog.conf
+            RUN sed 's/#$InputTCPServerRun 514/$InputTCPServerRun 514/' -i /etc/rsyslog.conf
+            EXPOSE 514/tcp 514/udp 
+            CMD ["/usr/sbin/rsyslogd", "-dn", "-f", "/etc/rsyslog.conf"]
+            RUN apt-get install -y openssh-server
+            RUN mkdir /var/run/sshd
+            RUN useradd -s /bin/bash -m virtshell
+            RUN echo "virtshell:virtshell" | chpasswd
+            RUN echo "virtshell ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+            RUN echo "root:root" | chpasswd
+            RUN sed -i "s/PermitRootLogin without-password/PermitRootLogin yes/" /etc/ssh/sshd_config
+            RUN sed "s@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g" -i /etc/pam.d/sshd
+            ENV NOTVISIBLE "in users profile"
+            RUN echo "export VISIBLE=now" >> /etc/profile
+            EXPOSE 22
+            CMD ["/usr/sbin/sshd", "-D"]
             '''
+
             dockerfile_binary = BytesIO(dockerfile.encode('utf-8'))
-            client = Client(base_url='tcp://127.0.0.1:2375')
             response = [line for line in client.build(fileobj=dockerfile_binary,
-                                                      rm=True, 
-                                                      tag='yourname/volume',
-                                                      container_limits={
-                                                        'memory': 1024 * 1024,
-                                                        'cpusetcpus': 1,
-                                                        'cpushares': 1000,
-                                                        'memswap': 1024*1024*8})]
+                                                      rm=True,
+                                                      tag='ubuntu/15.04')]
+            container = client.create_container(image='ubuntu:15.04')
+            container_id = container['Id']
+
+            client.start(container_id)
+            link_path = client.inspect_container(container_id)
+            ip = link_path['NetworkSettings']['IPAddress'])
             logger.info("docker-container %s created successfully." % name)
             request.message_log += "docker-container created successfully\n"
             request = start(request)
