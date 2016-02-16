de2e08ab0fe78b556e563b401b86367fb2ee20cc
Carlos Llano <carlos_llano@hotmail.com>
2016-02-15 20:59:53 -0500

    Modified provisioning server script to accept arguments

--- virtshell_server/scripts/provisioning_server.sh
+++ virtshell_server/scripts/provisioning_server.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
 echo "Installing docker..."
+sudo apt-get autoremove -y
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo rm /etc/apt/sources.list.d/docker.list
 sudo touch /etc/apt/sources.list.d/docker.list
 
-OS=$(lsb_release -si)
-VER=$(lsb_release -sr)
+if [ $# -gt 0 ]; then
+	OS="$1"
+	VER="$2"
+else
+	OS=$(lsb_release -si)
+	VER=$(lsb_release -sr)
+fi
+
 if [ "$OS" == "Ubuntu" ] && [ "$VER" == "15.04" ]; 
 then
     echo "Adding repository for Ubuntu 15.04"
-    echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" >> /etc/apt/sources.list.d/docker.list
+	sudo su -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-wily main' >> /etc/apt/sources.list.d/docker.list"    
 elif [ "$OS" == "Ubuntu" ] && [ "$VER" == "14.04" ];
 then
     echo "Adding repository for Ubuntu 14.04"
-    echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
+	sudo su -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' >> /etc/apt/sources.list.d/docker.list"    
 elif [ "$OS" == "Ubuntu" ] && [ "$VER" == "12.04" ]; 
 then
     echo "Adding repository for Ubuntu 12.04"
-    echo "deb https://apt.dockerproject.org/repo ubuntu-precise main" >> /etc/apt/sources.list.d/docker.list
+    sudo su -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-precise main' >> /etc/apt/sources.list.d/docker.list"
 else
     echo "Operating system not supported"
     exit 4
