901f5589f51c9e99e50ffdf32f6535e4afbd0ca9
Carlos Llano <carlos_llano@hotmail.com>
2016-02-17 21:56:16 -0500

    Added the integration between partitions and hosts

--- virtshell_server/virtshell_server/managment/hosts.py
+++ virtshell_server/virtshell_server/managment/hosts.py
@@ -1,4 +1,5 @@
 import uuid
+from managment.partitions import Partitions
 from managment.hosts_repository import HostsRepository
 
 class Hosts(object):
@@ -12,8 +13,13 @@ class Hosts(object):
         return self.hosts_repository.get_host(name)
 
     def create_host(self, host):
-        host['uuid'] = str(uuid.uuid4())
-        return self.hosts_repository.create_host(host)
+        partitions = Partitions()
+        result = partitions.add_host(host['partition'], host['name'])
+        if result['status'] == 'ok':
+            host['uuid'] = str(uuid.uuid4())
+            return self.hosts_repository.create_host(host)
+        else:
+            return {"add_host": "error", "reason": result['reason']}
 
     def delete_host(self, name):
         return self.hosts_repository.delete_host(name)
